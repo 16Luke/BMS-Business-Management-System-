95a86eac46c28b835acd8785f2d2d0f260464138
16Luke <cannoneluca2003@gmail.com>
2025-10-26 00:26:20 +0200

    setup documentation

---
+++ doc/html/search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['management_20system_0',['BMS-Business-Management-System',['../md__r_e_a_d_m_e.html',1,'']]]
+];
---
+++ doc/html/search/pages_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['bms_20business_20management_20system_0',['BMS-Business-Management-System',['../md__r_e_a_d_m_e.html',1,'']]],
+  ['business_20management_20system_1',['BMS-Business-Management-System',['../md__r_e_a_d_m_e.html',1,'']]]
+];
